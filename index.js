68d344a68773ba8e9f807a51e0467f05446a6d98
minkhant21 <kaungminkhant6889@gmail.com>
2023-05-27 20:02:03 +0630

    HtmlRendering

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const helper = require('./helper')
+// Html Rendering
+const http = require('http')
 const fs = require('fs')
-console.log("start Program")
-const file = fs.readFile('index.txt',(error,data)=>{
-    // if (e) {
-    //     throw e
-    // }
-    console.log(data.toString())
-   
+
+http.createServer((req,res)=>{
+    fs.readFile('./pages/index.html',(error,data)=>{
+        res.write(data)
+        res.end()
+    })
+}).listen(9000,()=>{
+    console.log("localhost:9000")
 })
-// const sum = () => {
-//     console.log(helper.sum(1,2))
-// }
-console.log(sum()+'h')
-console.log(helper.isLiked)
-console.log("End Program")
+
+// Http server 
+
+// const http = require('http');
+// http.createServer((res,req)=>{
+//     req.end('End Node Run')
+// }).listen(9000,()=>{
+//     console.log('localhost : 9000')
+// })
+
+//File read model Aync
+
+// const helper = require('./helper')
+// const fs = require('fs')
+// console.log("start Program")
+// const file = fs.readFile('index.txt',(error,data)=>{
+//     // if (e) {
+//     //     throw e
+//     // }
+//     console.log(data.toString())
+   
+// })
+// // const sum = () => {
+// //     console.log(helper.sum(1,2))
+// // }
+// console.log(sum()+'h')
+// console.log(helper.isLiked)
+// console.log("End Program")
+
 
