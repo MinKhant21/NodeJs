a581d29f35af86136ed133457ad6ee830ca65c60
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-01-23 11:54:52 +0630

    del

--- src/routes/productRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router, express } from "express";
-import * as productController from '../controller/productController'
-
-const baseRouter = Router();
-
-baseRouter.get('/',productController.getAll);
-baseRouter.post('/add',productController.add);
-
-module.exports = baseRouter
